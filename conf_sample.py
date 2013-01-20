ddb2f7169a4b32c9319ac28f555e3d8d4d9f7982
Raphaël Gaziano <r.gaziano@gmail.com>
2013-01-20 22:33:45 +0100

    Stoopid syntax errors in conf...

--- conf_sample.py
+++ conf_sample.py
@@ -6,21 +6,23 @@ GOOGLE_PSWRD = ""
 MAIL_LOGIN = ""
 MAIL_PSWRD = ""
 
+MAX_FILE_SIZE = "???"
+
 ADRESSES = dict(
-    bob: "bob@bob.com",
-    marylou: "hello@zerg.net",
-    elvis: "one4themon3y@pelvis.org",
-    vader: "wildchild@sithlordz.gouv",
-    blondie: "sonofa***@ponchos.com"
+    bob     = "bob@bob.com",
+    marylou = "hello@zerg.net",
+    elvis   = "one4themon3y@pelvis.org",
+    vader   = "wildchild@sithlordz.gouv",
+    blondie = "sonofa***@ponchos.com"
 )
 
-GROUPS = dict
-    pals: [
+GROUPS = dict(
+    pals = [
         "bob",
         "vader",
         "marylou",
     ],
-    work: [
+    work = [
         "blondie",
         "elvis",
         "bob"
@@ -28,9 +30,9 @@ GROUPS = dict
 )
 
 MAIL_TMPLS = dict(
-    tongs: dict(
-        header: "Yo!",
-        body: '''Obi-Wan bantha Luke Skywalker R2D2 rebel spies. 
+    tongs = dict(
+        header = "Yo!",
+        body   = '''Obi-Wan bantha Luke Skywalker R2D2 rebel spies. 
         I find your lack of faith disturbing sith lord help me Obi-Wan 
         Kenobi jawa bacta. Endor do or do not Alderaan Princess Leia 
         Organa I am your father. Scoundrel bullseye womp rats in my 
@@ -38,8 +40,8 @@ MAIL_TMPLS = dict(
         to rescue you may the Force be with you trench run the Force 
         star systems.'''
     ),
-    another: dict(
-        header: "popopo",
-        body: "GNAAAAAAAAAAAANANZNZOIJ"
+    another = dict(
+        header = "popopo",
+        body   = "GNAAAAAAAAAAAANANZNZOIJ"
     )
 )
