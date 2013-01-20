deb482655d7cbee23a0131c9d62b1d3cbb9b77b7
Raphaël Gaziano <r.gaziano@gmail.com>
2013-01-21 00:43:23 +0100

    get_recipients's first draft ok

--- sendcrap/tests/test_recipients.py
+++ sendcrap/tests/test_recipients.py
@@ -5,12 +5,54 @@ Recipients test file.
 Test the retrieving of a recipients list.
 """
 import unittest
+from sendcrap.utils import get_recipients
+import conf
 
 class TestRecipients(unittest.TestCase):
-    '''Archiving/Compression Tests'''
+    '''Recipient retrieval Tests'''
     def setUp(self): pass
     def tearDown(self): pass
+    
+    def _check_recipients(self, expected, received):
+        '''Common helper for the individual tests''' 
+        self.assertTrue(all([e in received for e in expected]))
+        self.assertTrue(all([r in expected for r in received]))
+    
+    def test_no_input(self):
+        '''get_recipients() (No args) should return an empty list'''
+        self.assertEqual([], get_recipients())
         
+    def test_get_group(self):
+        '''get_recipients(grp) should return the requested group's adresses as a list'''
+        for grp, c_list in conf.GROUPS.items():
+            expected = [conf.ADRESSES[c] for c in c_list]
+            self._check_recipients(expected, get_recipients(grp))
+            
+    def test_get_contacts(self):
+        '''get_recipients(contacts=[...]) should return the requested contacts' adresses'''
+        contacts = conf.ADRESSES.keys()
+        expected = conf.ADRESSES.values()
+        self._check_recipients(expected, 
+                               get_recipients(contacts=contacts))
+        contacts = contacts[:1] + contacts[-1:]
+        expected = expected[:1] + expected[-1:]
+        self._check_recipients(expected, 
+                               get_recipients(contacts=contacts))
+                               
+    def test_get_arbs(self):
+        '''get_recipients(arbs=[...]) should return only the provided mail adresses'''
+        expected = [
+            'dummy@dumdum.net',
+            'greenturnip@rattleboobies.com',
+            'listless.porpoise@zogzog.org'
+        ]
+        self._check_recipients(expected, get_recipients(arbs=expected))
+
+    # grp + contacts
+    # grp + arbs
+    # contacts + arbs
+    # all
+
 
 def suite():
     suite = unittest.TestSuite()
--- sendcrap/utils.py
+++ sendcrap/utils.py
@@ -10,22 +10,23 @@ Created: 19/01/2013
 Version: 1.0
 """
 import os
+import conf
     
-def list_files(dir_=None, walk=False, exts=None, arb_files=None):
+def list_files(dir_=None, walk=False, exts=None, arbs=None):
     '''
     Return a list of all the files meeting the provided criterias.
     
-    @param dir_:      Optional. Search directory.
-    @param walk:      Boolean indicating if subdirectories should be 
-                      searched. Defaults to False.
-    @param exts:      Optional. List of valid file extensions
-    @param arb_files: Optional. List of arbitrary files to be included.
-                      Those files will be added to the selection no 
-                      matter what.
-    @returns:         List of relative paths to all valid files.
+    @param dir_: Optional. Search directory.
+    @param walk: Boolean indicating if subdirectories should be 
+                 searched. Defaults to False.
+    @param exts: Optional. List of valid file extensions
+    @param arbs: Optional. List of arbitrary files to be included.
+                 Those files will be added to the selection no 
+                 matter what.
+    @returns:    List of relative paths to all valid files.
     '''
     if exts is None: exts = []
-    if arb_files is None: arb_files = []
+    if arbs is None: arbs = []
     all_files = []
     if dir_ is not None:
         if walk:
@@ -40,12 +41,22 @@ def list_files(dir_=None, walk=False, exts=None, arb_files=None):
                 if os.path.isfile(p):
                     all_files.append(p)
     # Add arbitrary files
-    for f in arb_files:
+    for f in arbs:
         all_files.append(f)
         
     return all_files
 
-def get_recipients():
+def get_recipients(grp="", contacts=None, arbs=None):
     '''TODO: tests and impl'''
-    pass
+    if contacts is None: contacts = []
+    if arbs is None: arbs = []
+    all_recs = []
+    for contact in conf.GROUPS.get(grp, []):
+        all_recs.append(conf.ADRESSES[contact])
+    for contact in contacts:
+        all_recs.append(conf.ADRESSES[contact])
+    for a in arbs:
+        all_recs.append(a)
+    
+    return all_recs
     
