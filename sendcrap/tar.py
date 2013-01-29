4f77851bc0ebdc43f4eba050320d2e17972f53ff
Raphaël Gaziano <r.gaziano@gmail.com>
2013-01-29 10:59:35 +0100

    tar.write doc

--- sendcrap/tar.py
+++ sendcrap/tar.py
@@ -19,14 +19,27 @@ def check_size(*files): pass
 
 # @TODO: try using shutils.make_archive
 def write(path, *files):
-    '''DOC'''
+    '''
+    Collects the given files into a single archive.
+    Will issue a warning and eventually terminate if the overall files
+    size considered too great (according to the conf.SIZE_WARN 
+    constant).
+    
+    @param path:  The path of root directory. Archive will be created 
+                  there and named after it.
+    @param files: Pathes of all the files to be collected, provided as
+                  a variable number of strings.
+    @return:      Path to the created archive.
+    '''
     dirname = os.path.basename(path)
-    tarname = dirname + '.tar'
+    tarname = '%s.tar' % dirname
+    tar_path = os.path.join(path, tarname)
+    # @TODO: check for size
+    utils.output('Creating archive %s...' % tarname) 
     # Using mode 'w:gz' adds gzip compression, but nests
     # the tar inside the zip.
-    utils.output('Creating archive %s...' % tarname) 
-    with tarfile.open(os.path.join(path, tarname), 'w') as tf:
+    with tarfile.open(tar_path, 'w') as tf:
         for f in files:
             utils.verbose_output("Adding %s to %s..." % (f, tarname))
             tf.add(f)
-    return tf
+    return tar_path
