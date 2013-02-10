543a2db1ee4a824a3d8ee348b44efdc8d2ed62c5
Raphaël Gaziano <r.gaziano@gmail.com>
2013-02-11 00:43:42 +0100

    mail sending 1st draft - NO error handling ATALL!

--- sendcrap/mail.py
+++ sendcrap/mail.py
@@ -9,6 +9,15 @@ Author:  raphi <r.gaziano@gmail.com>
 Created: 06/02/2013
 Version: 1.0
 """
+import smtplib
+import os
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email.Utils import COMMASPACE, formatdate
+from email import Encoders
+import getpass
+
 import conf
 from . import utils
 
@@ -53,7 +62,7 @@ def type_mail(_print_header_help=True):
     elif header in conf.MAIL_TMPLS.keys(): 
         return conf.MAIL_TMPLS[header]
     
-    utils.forced_output('quick help aboot body (explicit \\n!)')
+    utils.forced_output('quick help aboot body (empty line to stop!)')
     body = []
     while True:
         if not body: prompt = 'body: '
@@ -64,4 +73,45 @@ def type_mail(_print_header_help=True):
     return {'header': header, 'body': "\n".join(body)}
     
 def gen_mail(tmpl): pass 
-def send_mail(*dummyargs): pass
+
+def send_mail(tmpl, recipients, file_ ):
+    # TESTTESTTEST ###
+    # inspired:
+    # http://www.dzone.com/snippets/send-email-attachments-python
+    assert type(recipients) == list
+
+    msg = MIMEMultipart()
+    msg['From'] = 'r.gaziano@gmail.com'
+    msg['To'] = COMMASPACE.join(recipients)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = tmpl['header']
+
+    msg.attach( MIMEText(tmpl['body']) )
+
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(open(file_, 'rb').read())
+    Encoders.encode_base64(part)
+    part.add_header('Content-Disposition',
+           'attachment; filename="%s"' % os.path.basename(file_))
+    msg.attach(part)
+
+    smtp = smtplib.SMTP(conf.SMTP_SERVR, conf.SMTP_PORT)
+    # Try and send mail directly, in case the server doesn't require
+    # authentiication (is that even possible?)
+    #~ try:
+        #~ res = smtp.sendmail(conf.SENDER_EMAIL, recipients, 
+                            #~ msg.as_string())
+    #~ except smtplib.SMTPAuthenticationError:
+    smtp.starttls()
+    smtp.ehlo()
+    login = conf.MAIL_LOGIN or utils.input("mail login: ")
+    pswd  = conf.MAIL_PSWRD or getpass.getpass("mail pswd: ")
+    smtp.login(login, pswd)
+    res = smtp.sendmail(conf.SENDER_EMAIL, recipients, 
+                        msg.as_string())
+    smtp.close()
+    # res contains 
+    # "a dictionary, with one entry for each recipient that was refused. 
+    # Each entry contains a tuple of the SMTP error code and the 
+    # accompanying error message sent by the server."
+    print(res)
