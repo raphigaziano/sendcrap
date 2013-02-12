83c6a07ceb91d1911e6e13c8095680bd015f687c
Raphaël Gaziano <r.gaziano@gmail.com>
2013-02-12 17:54:54 +0100

    messy refacto in mail.py

--- sendcrap/mail.py
+++ sendcrap/mail.py
@@ -11,15 +11,28 @@ Version: 1.0
 """
 import smtplib
 import os
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEBase import MIMEBase
-from email.MIMEText import MIMEText
-from email.Utils import COMMASPACE, formatdate
-from email import Encoders
+import re
 import getpass
+try: # py2
+    from email.MIMEMultipart import MIMEMultipart
+    from email.MIMEBase import MIMEBase
+    from email.MIMEText import MIMEText
+    from email.Utils import COMMASPACE, formatdate
+    from email import Encoders
+except ImportError: #py3
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.base import MIMEBase
+    from email.mime.text import MIMEText
+    from email.utils import COMMASPACE, formatdate
+    import email.encoders as Encoders
 
 import conf
 from . import utils
+
+__ALL__ = ['get_template', 'send_mail']
+
+### Template Management ###
+###########################
 
 def get_template(tmpl):
     '''
@@ -62,54 +75,90 @@ def type_mail(_print_header_help=True):
     elif header in conf.MAIL_TMPLS.keys(): 
         return conf.MAIL_TMPLS[header]
     
-    utils.forced_output('quick help aboot body (empty line to stop!)')
+    utils.forced_output('quick help aboot body (empty line to stop + url placeholder + \n for blank lines!)')
     body = []
     while True:
-        if not body: prompt = 'body: '
-        else:        prompt = ' >>>  '
-        line = utils.input(prompt)
+        prompt = 'body: ' if not body else '  >>> '
+        line = utils.input(prompt).replace('\\n', ' ')
         body.append(line)
         if not line: break
     return {'header': header, 'body': "\n".join(body)}
     
-def gen_mail(tmpl): pass 
+### Email Handling ###
+######################
 
-def send_mail(tmpl, recipients, file_ ):
-    # TESTTESTTEST ###
-    # inspired:
-    # http://www.dzone.com/snippets/send-email-attachments-python
-    assert type(recipients) == list
-
-    msg = MIMEMultipart()
-    msg['From'] = 'r.gaziano@gmail.com'
-    msg['To'] = COMMASPACE.join(recipients)
-    msg['Date'] = formatdate(localtime=True)
+HTML_URL_TMPL = "<a href='%s' target='_blank'>%s</a>"
+URL_TAG_REGXP = re.compile(r'''[\n]?    # optional pre line break
+                               %s       # actual url tag
+                               [\n]?    # optional post line break
+                               ''', re.VERBOSE)
+    
+def gen_mail(tmpl, recipients, file_):
+    ''' '''
+    msg = MIMEMultipart('alternative')
+    msg['From']    = conf.SENDER_EMAIL
+    msg['To']      = COMMASPACE.join(recipients)
+    msg['Date']    = formatdate(localtime=True)
     msg['Subject'] = tmpl['header']
 
-    msg.attach( MIMEText(tmpl['body']) )
+    # File on the local hard-drive: send it along as an attachment
+    if utils.valid_local_path(file_):
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(open(file_, 'rb').read())
+        Encoders.encode_base64(part)
+        part.add_header('Content-Disposition',
+               'attachment; filename="%s"' % os.path.basename(file_))
+        msg.attach(part)
+        # Remove url tag from the template
+        tmpl['body'] = re.sub(URL_TAG_REGXP, '', tmpl['body'])
+    # File online: inject url in the template
+    elif utils.valid_http_url(file_):
+        html_url  = HTML_URL_TMPL % (file_, file_)
+        html_body = tmpl['body']
+        if '%s' in tmpl['body']:
+            tmpl['body'] = tmpl['body'] % file_
+            html_body    = html_body % html_url
+        else:
+            # append to end
+            tmpl['body'] += '\n%s' % file_
+            html_body    += '\n%s' % html_url
+        msg.attach( MIMEText(html_body, 'html'))
+            
+    msg.attach( MIMEText(tmpl['body'], 'plain'))
+    
+    return msg
 
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload(open(file_, 'rb').read())
-    Encoders.encode_base64(part)
-    part.add_header('Content-Disposition',
-           'attachment; filename="%s"' % os.path.basename(file_))
-    msg.attach(part)
+class SMTPSender(object):
+    '''
+    Context manager to handle errors related to the sending of emails.
+    Errors should be handled in the __exit__ method.
+    '''
+    def __enter__(self):
+        self.smtp = smtplib.SMTP(conf.SMTP_SERVR, conf.SMTP_PORT)
+        # try and connect ?
+        return self.smtp
 
-    smtp = smtplib.SMTP(conf.SMTP_SERVR, conf.SMTP_PORT)
-    # Try and send mail directly, in case the server doesn't require
-    # authentiication (is that even possible?)
-    #~ try:
-        #~ res = smtp.sendmail(conf.SENDER_EMAIL, recipients, 
-                            #~ msg.as_string())
-    #~ except smtplib.SMTPAuthenticationError:
-    smtp.starttls()
-    smtp.ehlo()
-    login = conf.MAIL_LOGIN or utils.input("mail login: ")
-    pswd  = conf.MAIL_PSWRD or getpass.getpass("mail pswd: ")
-    smtp.login(login, pswd)
-    res = smtp.sendmail(conf.SENDER_EMAIL, recipients, 
-                        msg.as_string())
-    smtp.close()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # Tempo:
+        print exc_type
+        print exc_val
+        print exc_tb
+        
+        self.smtp.close()
+        return True # Prevent exception propagation
+
+def send_mail(tmpl, recipients, file_ ):
+    ''' '''
+    msg = gen_mail(tmpl, recipients, file_)
+
+    with SMTPSender() as smtp:
+        smtp.starttls()
+        smtp.ehlo()
+        login = conf.MAIL_LOGIN or utils.input("mail login: ")
+        pswd  = conf.MAIL_PSWRD or getpass.getpass("mail pswd: ")
+        smtp.login(login, pswd)
+        res = smtp.sendmail(conf.SENDER_EMAIL, recipients, 
+                            msg.as_string())
     # res contains 
     # "a dictionary, with one entry for each recipient that was refused. 
     # Each entry contains a tuple of the SMTP error code and the 
